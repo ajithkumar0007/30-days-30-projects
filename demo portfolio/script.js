ee109ce97a64a32038ec06fb9d4cad3022bfb5a9
Ajith6667 <95462465+ajithkumar0007@users.noreply.github.com>
2023-08-23 09:49:35 +0530

    Dark theme

---
+++ demo portfolio/script.js
@@ -0,0 +1,23 @@
+var icon = document.getElementById("icon");
+if (localStorage.getItem("theme") == null) {
+  localStorage.setItem("theme", "light");
+}
+
+let localData = localStorage.getItem("theme");
+if (localData == "light") {
+  icon.src = "./portfolio_site_img/dark theme icon/moon.png";
+  document.body.classList.remove("dark-theme");
+} else if (localData == "dark") {
+  icon.src = "./portfolio_site_img/dark theme icon/sun.png";
+  document.body.classList.add("dark-theme");
+}
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./portfolio_site_img/dark theme icon/sun.png";
+    localStorage.setItem("theme", "dark");
+  } else {
+    icon.src = "./portfolio_site_img/dark theme icon/moon.png";
+    localStorage.setItem("theme", "light");
+  }
+};
